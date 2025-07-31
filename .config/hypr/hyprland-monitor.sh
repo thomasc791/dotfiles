7db48a6adf0338fc0ff7b31252dfd68979b73afa
Thomas Arch <thomasc791@gmail.com>
2025-07-31 14:45:04 +0200

    corrected hyprland

--- .config/hypr/hyprland-monitor.sh
+++ .config/hypr/hyprland-monitor.sh
@@ -9,9 +9,12 @@ set_monitors() {
       index="${monitors[(ie)$monitor]}"
       size=$(echo ${sizes[index]} | sed "s/.*: //" | awk '{print $1}' )
 
-      hyprctl keyword monitor $monitor, $size, 0x-$height, 1
       sizeHeight=$(echo $size | sed "s/.*x\(.*\)@.*/\1/")
-      height=$(($height+$sizeHeight ))
+      if [[ $index != "1" ]]; then
+         height=$(($height+$sizeHeight ))
+      fi
+
+      hyprctl keyword monitor $monitor, $size, 0x-$height, 1
    done
 }
 
